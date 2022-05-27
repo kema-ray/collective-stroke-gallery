872e09a1cdc24df588eb4b6e529f2217623f7e5b
rachel <rachelkemuma99@gmail.com>
2022-05-27 13:22:47 +0300

    make changes to home.html

--- gallery/settings.py
+++ gallery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'rachel',
+        'PASSWORD': 'hotspurs',
     }
 }
 
