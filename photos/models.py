2ff9cc4b6c569d41c401479d87f7d7bc2e7836e2
rachel <rachelkemuma99@gmail.com>
2022-05-27 13:43:56 +0300

    create class photos in models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Photos(models.Model):
+    image_name = models.CharField(max_length=30)
+    description = models.TextField(max_length=100, blank=False)
