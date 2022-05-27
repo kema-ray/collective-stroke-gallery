886786b5e2da5401709eac5a2b87aff9a7d096f2
rachel <rachelkemuma99@gmail.com>
2022-05-27 13:56:17 +0300

    create class category and make migrations

--- photos/models.py
+++ photos/models.py
@@ -1,6 +1,16 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
 class Photos(models.Model):
     image_name = models.CharField(max_length=30)
     description = models.TextField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.image_name
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
