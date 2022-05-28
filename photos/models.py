5133694b638976c823b004a28650ae884b927e0a
rachel <rachelkemuma99@gmail.com>
2022-05-28 19:21:01 +0300

    add image uploading functionality to class photos

--- photos/models.py
+++ photos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Photos(models.Model):
     image_name = models.CharField(max_length=30)
     description = models.TextField()
+    image = models.ImageField(upload_to = 'pictures/', default='No image')
     photo_category = models.ForeignKey('Category',on_delete=models.CASCADE,default='')
     photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL,default = '', null = True)
     pub_date = models.DateTimeField(auto_now_add=False,auto_now= True)
@@ -28,6 +29,6 @@ class Location(models.Model):
 
     def save_location(self):
         self.save()
-        
+
     def __str__(self):
         return self.location
