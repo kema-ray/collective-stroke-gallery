e19774c838f25962444f5d1d60f1510d5fd860a2
rachel <rachelkemuma99@gmail.com>
2022-05-28 15:58:30 +0300

    adding save function to class photos,category and location

--- photos/models.py
+++ photos/models.py
@@ -8,11 +8,17 @@ class Photos(models.Model):
     photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL,default = '', null = True)
     pub_date = models.DateTimeField(auto_now_add=False,auto_now= True)
 
+    def save_photo(self):
+        self.save()
+
     def __str__(self):
         return self.image_name
 
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
+
+    def save_category_name(self):
+        self.save()
 
     def __str__(self):
         return self.category_name
@@ -20,5 +26,8 @@ class Category(models.Model):
 class Location(models.Model):
     location = models.CharField(max_length=30)
 
+    def save_location(self):
+        self.save()
+        
     def __str__(self):
         return self.location
