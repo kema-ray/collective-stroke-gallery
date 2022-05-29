f1ca775f8ca2463ab477c76a5b7f05170287f6f9
rachel <rachelkemuma99@gmail.com>
2022-05-29 09:44:54 +0300

    displaying the cards

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime as dt
+from .models import Photos
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    # date = dt.date.today()
+    images = Photos.objects.all()
+    return render(request,'home.html',{"images":images})
     # return HttpResponse('Welcome to the photo gallery')
